f592cde90701d5c69228fe782fdf4b3021f1d6c1
pinkocevn <pinkocevn@gmail.com>
2020-01-29 17:00:42 +0900

    Update musinsa_mb.js

--- js/musinsa_mb.js
+++ js/musinsa_mb.js
@@ -237,11 +237,7 @@ $(function() {
         }else{
             confirm('최근검색어를 모두 삭제하시겠습니까?');
             alert('삭제되었습니다.');
-            $('.list-item-wrap').empty().text('최근검색어가 없습니다.').css({
-                'text-align':'center',
-                'font-size':'12px',
-                'color':'#1b1b1b'
-            });
+            $('.list-item li a').empty();
         };
     })
 
